9a28ae8e8223ffdb471de8a6096f773161dd38d0
Leandro Ron Emanuel Aeschbacher <leandro.aeschbacher77@gmail.com>
2025-08-26 09:42:06 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.routers import api_routes, ui_routes
+
+app = FastAPI(title="Printer API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+app.include_router(api_routes.router, prefix="", tags=["API"])
+app.include_router(ui_routes.router, prefix="", tags=["UI"])
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
