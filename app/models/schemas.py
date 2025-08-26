be5d44ec71dfd61752e2ad46d8954f76b844264a
Leandro Ron Emanuel Aeschbacher <leandro.aeschbacher77@gmail.com>
2025-08-26 09:40:14 +0200

    Update schemas.py

--- app/models/schemas.py
+++ app/models/schemas.py
@@ -1,1 +1,13 @@
+from pydantic import BaseModel
+from typing import List
+
+class PrintPayload(BaseModel):
+    title: str = "TASKS"
+    lines: List[str] = []
+    cut: bool = True
+    add_datetime: bool = True
+
+class RawPayload(BaseModel):
+    text: str
+    add_datetime: bool = False
 
